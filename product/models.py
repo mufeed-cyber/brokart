5998707d9995298f3adeae2a0bb8d5ee65f47761
Mufeed <mufeedgvr@gmail.com>
2024-07-26 14:45:42 +0530

    model completed

--- product/models.py
+++ product/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+# Create your model for product
+class Product(models.Model):
+    LIVE=1
+    DELETE=0
+    DELETE_CHOICES=((LIVE,'Live'),(DELETE,'Delete'))
+    title=models.CharField(max_length=250)
+    price=models.FloatField()
+    description=models.TextField()
+    image=models.ImageField(upload_to='media/')
+    priority=models.IntegerField(default=0)
+    delete_choices=models.IntegerField(choices=DELETE_CHOICES,default=LIVE)
+    created_at=models.DateTimeField(auto_now_add=True)
+    updated_at=models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.title
